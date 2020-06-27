621e0af82a10d5f66fc4b3434088128308c9ee7c
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-06-27 12:14:17 +0200

    Changed Package Script

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import {app} from './app'
 
 const PORT = process.env.PORT || 3000
 
-console.log("🏃‍♂️Running backend...")
+console.log("🏃‍Running backend...")
 
 app.listen(PORT, () =>{
     console.log("🏁Backend is now running on port " + PORT)
